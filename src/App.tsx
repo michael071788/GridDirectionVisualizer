06ffc36d71c31167d5da9c8aa1dba372d61da153
michael071788 <michael071788@gmail.com>
2024-10-16 06:34:55 +0800

    Added the main GridDirectionVisualizer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
+import {Container, Grid2, Box, TextField, InputLabel  } from '@mui/material';
+import GridDirectionVisualizer from './components/GridDirectionVisualizer';
+
 
 function App() {
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [initialLabelMessage, setInitialLabelMessage] = useState("Please input direction on the following format: \"x,y direction\n (e.g., \"1,1 NORTH\")...");
+  
+  const [inputDirection, setInputDirection] = useState("");
+  const [isInputDirectionExecuted, setIsInputDirectionExecuted] = useState(false);
+
+  const handleInputOnChange = (e: any) => {
+    setInputDirection(e.target.value.toUpperCase());
+}
+
+  //It's just my best practice to put some delay when typing in the input field.
+  //Just to make sure, especially when this kind of approach is being used on fetching or sending a request to the API :)
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if(inputDirection !== "")  {
+        setIsInputDirectionExecuted(true);
+      } else {
+
+        setIsInputDirectionExecuted(false)
+      }
+    }, 1000)
+
+    return () => clearTimeout(delayDebounceFn)
+  }, [inputDirection])
+
+  useEffect(() => {
+  }, [isInputDirectionExecuted])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Container maxWidth="sm">
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh'
+        }}
+      >
+        <Grid2 container direction="column" spacing={2} alignItems="center">
+          <Grid2 >
+            <TextField id="outlined-basic" label="Input direction..." variant="outlined" onChange={handleInputOnChange} />
+          </Grid2>
+
+          {!isInputDirectionExecuted ? 
+          ( <Grid2 >
+            <InputLabel sx={{background: '#aae6f7',fontSize: '25px'}} >{initialLabelMessage}</InputLabel >
+          </Grid2>) 
+          : 
+          (<Grid2 >
+            <GridDirectionVisualizer position={inputDirection} />
+          </Grid2>)}
+          
+        </Grid2>
+      </Box>
+    </Container>
     </div>
   );
 }
